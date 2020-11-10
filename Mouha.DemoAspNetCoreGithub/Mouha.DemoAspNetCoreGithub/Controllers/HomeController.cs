e6db3788b07266113f3602590a5f90e36314d73f
Azure Demo <mouhameddiouf19@gmail.com>
2020-11-10 13:10:25 -0500

    Ajout mvc application

---
+++ Mouha.DemoAspNetCoreGithub/Mouha.DemoAspNetCoreGithub/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mouha.DemoAspNetCoreGithub.Controllers
+{
+    public class HomeController: Controller
+    {
+        public string Index()
+        {
+            return "Bonjour Mouha!";
+        }
+    }
+}
--- Mouha.DemoAspNetCoreGithub/Mouha.DemoAspNetCoreGithub/Startup.cs
+++ Mouha.DemoAspNetCoreGithub/Mouha.DemoAspNetCoreGithub/Startup.cs
@@ -16,6 +16,7 @@ namespace Mouha.DemoAspNetCoreGithub
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -78,40 +79,42 @@ namespace Mouha.DemoAspNetCoreGithub
            
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    #region "Cours sur les variables environnement"
-                    //if (env.IsDevelopment())
-                    //{
-                    //    await context.Response.WriteAsync("Bonjour environnement dev!");
-                    //}
-                    //else if (env.IsProduction())
-                    //{
-                    //    await context.Response.WriteAsync("Bonjour environnement prod!");
-                    //}
-                    //else if (env.IsStaging())
-                    //{
-                    //    await context.Response.WriteAsync("Bonjour environnement stag!");
-                    //}
-                    //else
-                    //{
-                    //    await context.Response.WriteAsync(env.EnvironmentName);
-                    //}
+                endpoints.MapDefaultControllerRoute();
 
-                    //if (env.IsEnvironment("Develop"))
-                    //{
-                    //    await context.Response.WriteAsync("Bonjour au particulier nom!");
-                    //}
-                    //else
-                    //{
-                    //    await context.Response.WriteAsync(env.EnvironmentName);
-                    //}
+                //endpoints.MapGet("/", async context =>
+                //{
+                #region "Cours sur les variables environnement"
+                //if (env.IsDevelopment())
+                //{
+                //    await context.Response.WriteAsync("Bonjour environnement dev!");
+                //}
+                //else if (env.IsProduction())
+                //{
+                //    await context.Response.WriteAsync("Bonjour environnement prod!");
+                //}
+                //else if (env.IsStaging())
+                //{
+                //    await context.Response.WriteAsync("Bonjour environnement stag!");
+                //}
+                //else
+                //{
+                //    await context.Response.WriteAsync(env.EnvironmentName);
+                //}
+
+                //if (env.IsEnvironment("Develop"))
+                //{
+                //    await context.Response.WriteAsync("Bonjour au particulier nom!");
+                //}
+                //else
+                //{
+                //    await context.Response.WriteAsync(env.EnvironmentName);
+                //}
 
                     #endregion
 
-                    await context.Response.WriteAsync("Hello World!");
+                //    await context.Response.WriteAsync("Hello World!");
 
-                });
+                //});
             });
 
             
