118a4b6dbc9f842da3907cd2f55f208f6f6cc204
Azure Demo <mouhameddiouf19@gmail.com>
2020-11-10 13:55:52 -0500

    Ajout controller book

---
+++ Mouha.DemoAspNetCoreGithub/Mouha.DemoAspNetCoreGithub/Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mouha.DemoAspNetCoreGithub.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "Tous les livres";
+        }
+
+        public string GetBook(int id)
+        {
+            return $"Livre avec id = {id}";
+        }
+
+        public string SearchBook(string bookName, string authorName)
+        {
+            return $"Livre avec nom = {bookName} & Auteur = {authorName}";
+        }
+    }
+}
